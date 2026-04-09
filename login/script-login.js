4cac195eea13f3185a024ba3378a9795a9b9c3c4
marysia <maryska3107@wp.pl>
2026-04-09 18:53:01 +0200

    init

--- login/script-login.js
+++ login/script-login.js
@@ -1,8 +1,7 @@
 const form = document.getElementById('loginForm');
 const msg = document.getElementById('loginMsg');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+function login() {
     const username = document.getElementById('username').value.trim();
     const password = document.getElementById('password').value;
 
@@ -12,11 +11,9 @@ form.addEventListener('submit', (e) => {
     };
 
     if (Object.prototype.hasOwnProperty.call(users, username) && password === users[username]) {
-        msg.textContent = `Zalogowano pomyslnie (${username}).`;
-        msg.style.color = 'green';
-        window.location.href = '../index.html';
+       window.location.href = '../index.html';
     } else {
         msg.textContent = 'Bledny login lub haslo.';
         msg.style.color = 'red';
     }
-});
+}
